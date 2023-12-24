5c93b13d34ef3899d6e1e731a096568a1bdec306
Grzegorz Sroda <grzegorz_sroda@wp.pl>
2023-12-24 09:56:46 +0100

    post entity created, ready to implement LOB

---
+++ src/main/java/pl/grzegorz/neat/model/post/PostService.java
@@ -0,0 +1,16 @@
+package pl.grzegorz.neat.model.post;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public interface PostService {
+    List<PostEntity> getAllPosts();
+
+    PostEntity getPostById(Long postId);
+
+    PostEntity createPost(PostEntity post);
+
+    void deletePost(Long postId);
+}
